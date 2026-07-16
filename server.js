e72d4449b0eada626990f58f16836816e474f203
bagasthebrave-alt <bagasthebrave@gmail.com>
2026-07-16 18:27:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -112,6 +112,8 @@ ${flights.map(flight => `
     
 }); //
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-}); 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
